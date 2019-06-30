64e21ca378395a3a509201d6e0d462f768176d4b
Ivan Butrim <vbutrim@gmail.com>
2019-06-30 20:46:55 +0300

    discard name logging in AccountNotEnoughMoneyException

--- src/main/java/storage/exceptions/AccountNotEnoughMoneyException.java
+++ src/main/java/storage/exceptions/AccountNotEnoughMoneyException.java
@@ -2,7 +2,7 @@ package storage.exceptions;
 
 public class AccountNotEnoughMoneyException extends RuntimeException {
 
-    public AccountNotEnoughMoneyException(String clientName, String ccy) {
-        super(String.format("Not enough money on '%s's '%s' account", clientName, ccy));
+    public AccountNotEnoughMoneyException(long id) {
+        super(String.format("Not enough money on Account %s", id));
     }
 }

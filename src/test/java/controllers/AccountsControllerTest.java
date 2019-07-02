8813f1f93c4105e7fe215f7a29f16f9e0a1c88e6
Ivan Butrim <vbutrim@gmail.com>
2019-07-02 23:19:34 +0300

    add classes for controllers tests

---
+++ src/test/java/controllers/AccountsControllerTest.java
@@ -0,0 +1,6 @@
+package controllers;
+
+public class AccountsControllerTest {
+
+    // TODO: provide tests
+}
---
+++ src/test/java/controllers/ClientsControllerTest.java
@@ -0,0 +1,6 @@
+package controllers;
+
+public class ClientsControllerTest {
+
+    // TODO: provide tests
+}

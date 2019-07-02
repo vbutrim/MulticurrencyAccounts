d92ea995e7534d1fcd671f20a854ae624469adeb
Ivan Butrim <vbutrim@gmail.com>
2019-07-03 02:43:44 +0300

    provide test for ClientsController

--- src/test/java/controllers/ClientsControllerTest.java
+++ src/test/java/controllers/ClientsControllerTest.java
@@ -1,6 +1,52 @@
 package controllers;
 
-public class ClientsControllerTest {
+import com.google.gson.Gson;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+import org.mockito.Mock;
+import services.ClientsService;
+import storage.data.Client;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ClientsControllerTest extends JerseyTest {
+
+    private static final Gson gson = new Gson();
+
+    @Mock
+    private ClientsService clientsService;
+
+    @Override
+    protected Application configure() {
+        clientsService = mock(ClientsService.class);
+
+        return new ResourceConfig()
+                .register(new ClientsController(clientsService));
+    }
+
+    @Test
+    public void shouldReturnAllClientsOnGet() {
+        // Given
+        List<Client> expectedClients = Collections.emptyList();
+        when(clientsService.getAllClients()).thenReturn(expectedClients);
+
+        // When
+        Response response = target("/clients").request().get();
+
+        // Then
+        verify(clientsService, times(1)).getAllClients();
+        assertEquals(gson.toJson(expectedClients), response.getEntity());
+    }
 
     // TODO: provide tests
 }
